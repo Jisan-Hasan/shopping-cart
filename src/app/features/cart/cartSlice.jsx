d4b6113f830852b98d36ef68721860fe09553f22
Jisan-Hasan <jisan.jh5@gmail.com>
2023-09-04 19:06:56 +0600

    basic redux store setup done

---
+++ src/app/features/cart/cartSlice.jsx
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    cart: [],
+};
+
+export const cartSlice = createSlice({
+    name: "cart",
+    initialState,
+    reducers: {
+        addToCart: (state, payload) => {
+            state.cart.push(payload);
+        },
+    },
+});
+
+export const { addToCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
